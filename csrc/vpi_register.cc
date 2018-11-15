93b27b35dae537386a8f6d6a319d6c67cf7bb0fc
Jimmy Situ <web@jimmystone.cn>
2018-11-15 23:19:31 +0800

    Add vpi register file

---
+++ csrc/vpi_register.cc
@@ -0,0 +1,19 @@
+/*
+ * VPI register
+ * -----------------------------------------------------------------------
+ * Jimmy Situ (web@jimmystone.cn)
+ */
+#include <vpi_user.h>
+#include <sv_vpi_user.h>
+
+
+/*
+ * This is a table of register functions. This table is the external
+ * symbol that the simulator looks for when loading this .vpi module.
+ */
+extern void block_device_tick_register();
+
+void (*vlog_startup_routines[])(void) = {
+      block_device_tick_register,
+      0
+};
